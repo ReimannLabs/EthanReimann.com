48e5af96f2a39173b07984ba28a6f53c45f4a8b3
Ethan Reimann <ereimann@reimannlabs.com>
2026-05-19 08:57:58 -0500

    feat: Add sitemap generation for the website

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,41 @@
+import { getCollection } from 'astro:content';
+
+export const prerender = true;
+
+const SITE_URL = 'https://ethanreimann.com';
+
+const staticPages = ['/', '/blog/', '/photography/', '/projects/', '/samling/'];
+
+function formatDate(date: Date) {
+  return date.toISOString().split('T')[0];
+}
+
+function sitemapEntry(path: string, lastmod?: Date) {
+  const url = new URL(path, SITE_URL);
+
+  return `  <url>
+    <loc>${url.href}</loc>${lastmod ? `
+    <lastmod>${formatDate(lastmod)}</lastmod>` : ''}
+  </url>`;
+}
+
+export async function GET() {
+  const posts = await getCollection('blog', ({ data }) => !data.draft);
+  const blogEntries = posts
+    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
+    .map((post) => sitemapEntry(`/blog/${post.id}/`, post.data.date));
+
+  const entries = [...staticPages.map((page) => sitemapEntry(page)), ...blogEntries];
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${entries.join('\n')}
+</urlset>`,
+    {
+      headers: {
+        'Content-Type': 'application/xml; charset=utf-8',
+      },
+    },
+  );
+}
